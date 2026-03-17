247543d07a1e1c2063e6d7fd6b12c8ac2bf0fc6e
saksshrivastava2211 <shrivastavas@cdmsmith.com>
2026-03-17 19:55:16 +0530

    removed unwanted code

--- src/features/feature_store.py
+++ src/features/feature_store.py
@@ -1,43 +1,4 @@
 # # src/features/feature_store.py
-# from databricks.feature_store import FeatureStoreClient
-# from pyspark.sql import DataFrame
-
-
-# def create_feature_table(df: DataFrame, config: dict):
-#     """
-#     Create or overwrite feature table
-#     """
-
-#     table_name = config["feature_store"]["table_name"]
-
-#     fs = FeatureStoreClient()
-
-#     try:
-#         fs.write_table(
-#             name=table_name,
-#             df=df,
-#             mode="overwrite"
-#         )
-#     except Exception:
-#         fs.create_table(
-#             name=table_name,
-#             primary_keys=["id"],
-#             df=df,
-#             description="Water quality engineered features"
-#         )
-
-
-# def read_feature_table(config: dict):
-#     """
-#     Read feature table
-#     """
-#     table_name = config["feature_store"]["table_name"]
-
-#     fs = FeatureStoreClient()
-
-#     return fs.read_table(name=table_name)
-
-
 from logging import config
 
 from databricks.feature_store import FeatureStoreClient
--- src/training/train_model.py
+++ src/training/train_model.py
@@ -12,7 +12,7 @@ from mlflow.models.signature import infer_signature
 
 def train_model(spark, config):
 
-    # ✅ Use Unity Catalog
+    # Use Unity Catalog
     mlflow.set_registry_uri("databricks-uc")
 
     mlflow.set_experiment(config["mlflow"]["experiment_name"])
@@ -20,14 +20,14 @@ def train_model(spark, config):
     env = config["env"]
     catalog = config["catalog"][env]
 
-    # ✅ model config
+    # model config
     schema = config["model_registry"]["schema"]
     model_name = config["model_registry"]["name"]
 
-    # ✅ ensure schema exists
+    #  ensure schema exists
     # spark.sql(f"CREATE SCHEMA IF NOT EXISTS {catalog}.{schema}")
 
-    # ✅ full UC model name
+    # full UC model name
     full_model_name = f"{catalog}.{schema}.{model_name}"
 
     df = read_feature_table(config).toPandas()
@@ -65,7 +65,7 @@ def train_model(spark, config):
                         input_example=X_train.iloc[:5]
                     )
 
-        # ✅ REGISTER MODEL (FIXED)
+        # REGISTER MODEL (FIXED)
         mlflow.register_model(
             f"runs:/{run.info.run_id}/model",
             full_model_name
